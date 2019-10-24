e0d4015739e930154aa34a52e599f6f0696664d2
cyberspacedk <frontend.dk@gmail.com>
2019-10-24 16:15:23 +0300

    remove odd code

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
-/* eslint-disable react/state-in-constructor */
-/* eslint-disable react/destructuring-assignment */
-/* eslint-disable react/button-has-type */
 import { hot } from 'react-hot-loader/root';
 import React from 'react';
+import './img/react_boilerplate.jpg';
 
-const Message = React.lazy(() => import('./warning.jsx'));
-
-class App extends React.Component {
-  state = {
-    counter: 0,
-  };
-
-  render() {
-    return (
-      <>
-        <h1 style={{ color: `${this.state.counter > 4 ? 'yellow' : 'blue'}` }}>
-          I React boilerplate
-        </h1>
-        <span>{this.state.counter}</span>
-        <button
-          onClick={() =>
-            this.setState(prevState => ({ counter: prevState.counter + 1 }))
-          }
-        >
-          +
-        </button>
-        <button
-          onClick={() =>
-            this.setState(prevState => ({ counter: prevState.counter - 1 }))
-          }
-        >
-          -
-        </button>
-        {this.state.counter === 5 && (
-          <React.Suspense fallback={null}>
-            <Message />
-          </React.Suspense>
-        )}
-      </>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="title">
+      <h1>React boilerplate</h1>
+      <img
+        className="boilerplate-image"
+        src="./img/react_boilerplate.jpg"
+        alt="react"
+      />
+    </div>
+  );
+};
 
 export default hot(App);
